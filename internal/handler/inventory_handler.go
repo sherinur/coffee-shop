f11bcf49d5d18b6b721e27ed16a28fd32f42e341
sherinur <sherinurislam@gmail.com>
2024-11-10 13:31:17 +0500

    Implemented error and json responses

--- internal/handler/inventory_handler.go
+++ internal/handler/inventory_handler.go
@@ -1,9 +1,12 @@
 package handler
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"hot-coffee/internal/service"
+	"hot-coffee/models"
 )
 
 type InventoryHandler interface {
@@ -22,10 +25,37 @@ func NewInventoryHandler(s service.InventoryService) InventoryHandler {
 	return &inventoryHandler{InventoryService: s}
 }
 
+// 200 OK — запрос был успешно обработан.
+// 201 Created — новый ресурс был успешно создан.
+// 400 Bad Request — ошибка в запросе.
+// 500 Internal Server Error — ошибка на сервере.
+
+func WriteJSONResponse(statusCode int, jsonResponse any, w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(statusCode)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(jsonResponse)
+}
+
+func WriteErrorResponse(statusCode int, err error, w http.ResponseWriter, r *http.Request) {
+	// TODO: Add ERROR log here, when error is writing
+	errorJSON := &models.ErrorResponse{Error: err.Error()}
+	WriteJSONResponse(statusCode, errorJSON, w, r)
+}
+
 func (h *inventoryHandler) AddInventoryItem(w http.ResponseWriter, r *http.Request) {
 	// TODO: implement logic to Add a new inventory item.
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("There will be INVENTORY ITEM CREATING"))
+	var item models.InventoryItem
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&item); err != nil {
+		WriteErrorResponse(http.StatusBadRequest, err, w, r)
+		return
+	}
+
+	fmt.Println(item)
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Item is successfully created."))
 }
 
 func (h *inventoryHandler) GetInventoryItems(w http.ResponseWriter, r *http.Request) {
