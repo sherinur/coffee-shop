52066b85cf06aa5068ef4f0a586483abc6e4e188
sherinur <sherinurislam@gmail.com>
2025-02-23 03:56:42 +0500

    Added Error Handling for inventory handler

--- internal/service/errors.go
+++ internal/service/errors.go
@@ -21,6 +21,15 @@ func (e *ServiceError) Unwrap() error {
 	return e.Err
 }
 
+// Hash returns the hash map of the ServiceError
+func (e *ServiceError) Hash() map[string]any {
+	return map[string]any{
+		"code":    e.Code,
+		"error":   e.Err.Error(),
+		"message": e.Message,
+	}
+}
+
 func NewServiceError(err error, code int, message string) *ServiceError {
 	return &ServiceError{
 		Err:     err,
@@ -30,12 +39,16 @@ func NewServiceError(err error, code int, message string) *ServiceError {
 }
 
 var (
+	// Inventory errors
+
 	ErrNotValidIngredientID   error = NewServiceError(errors.New("invalid ingredient ID"), http.StatusBadRequest, "ingredient ID is not valid")
-	ErrNotUniqueID            error = NewServiceError(errors.New("not unique ingredient ID"), http.StatusConflict, "ingredient ID must be unique")
+	ErrNotUniqueID            error = NewServiceError(errors.New("not unique ingredient ID"), http.StatusConflict, "item with the same ID already exists")
 	ErrNoItem                 error = NewServiceError(errors.New("item not found"), http.StatusNotFound, "item with the given ID does not exist")
 	ErrNotValidIngredientName error = NewServiceError(errors.New("invalid ingredient Name"), http.StatusBadRequest, "ingredient name is not valid")
 	ErrNotValidQuantity       error = NewServiceError(errors.New("invalid ingredient Quantity"), http.StatusBadRequest, "ingredient quantity is not valid")
 	ErrNotValidUnit           error = NewServiceError(errors.New("invalid ingredient Unit"), http.StatusBadRequest, "ingredient unit is not valid")
+
+	// Menu errors
 
 	ErrNotValidMenuID           error = errors.New("product ID is not valid")
 	ErrNotUniqueMenuID          error = errors.New("product ID must be unique")
@@ -44,6 +57,8 @@ var (
 	ErrNotValidPrice            error = errors.New("product price must be greater than 0")
 	ErrDuplicateMenuIngredients error = errors.New("the ingredients of the product must not be repeated")
 	ErrNotValidIngredints       error = errors.New("product ingredients is not valid")
+
+	// Order errors
 
 	ErrNotValidOrderID           error = errors.New("order ID is not valid")
 	ErrNotValidOrderCustomerName error = errors.New("order CustomeName is not valid")
--- internal/transport/http/handler/inventory_handler.go
+++ internal/transport/http/handler/inventory_handler.go
@@ -1,7 +1,7 @@
 package handler
 
 import (
-	"fmt"
+	"errors"
 	"god"
 	"log/slog"
 	"net/http"
@@ -10,28 +10,30 @@ import (
 	"coffee-shop/models"
 )
 
-type InventoryHandler interface {
+type InventoryWriter interface {
 	AddInventoryItem(*god.Context)
-	GetInventoryItems(*god.Context)
-	GetInventoryItem(*god.Context)
 	UpdateInventoryItem(*god.Context)
 	DeleteInventoryItem(*god.Context)
 }
 
+type InventoryReader interface {
+	GetInventoryItems(*god.Context)
+	GetInventoryItem(*god.Context)
+}
+
 type inventoryHandler struct {
-	InventoryService service.InventoryService
+	inventoryService service.InventoryService
 	log              *slog.Logger
 }
 
 func NewInventoryHandler(s service.InventoryService, l *slog.Logger) *inventoryHandler {
-	return &inventoryHandler{InventoryService: s, log: l}
+	return &inventoryHandler{inventoryService: s, log: l}
 }
 
 // AddInventoryItem handles the HTTP request to add a new inventory item.
 // It processes the incoming request, validates the input, and interacts with the service layer to add the item.
 // If successful, it returns the added item as a JSON response with a 201 status code.
 func (h *inventoryHandler) AddInventoryItem(c *god.Context) {
-	defer c.Request.Body.Close()
 	var item models.InventoryItem
 	err := c.ShouldBindJSON(&item)
 	if err != nil {
@@ -39,21 +41,10 @@ func (h *inventoryHandler) AddInventoryItem(c *god.Context) {
 		return
 	}
 
-	err = h.InventoryService.AddInventoryItem(item)
+	err = h.inventoryService.AddInventoryItem(item)
 	h.log.Debug("Adding new inventory item:", slog.Any("item", item))
 	if err != nil {
-		switch err {
-		case service.ErrNotUniqueID:
-			c.JSON(http.StatusConflict, god.H{"error": err.Error(), "message": "item with the same ID already exists"})
-			return
-		case service.ErrNotValidIngredientID, service.ErrNotValidIngredientName, service.ErrNotValidQuantity, service.ErrNotValidUnit:
-			c.JSON(http.StatusBadRequest, god.H{"error": err.Error(), "message": "invalid request body"})
-			return
-		default:
-			h.log.Error("Error of AddInventoryItem", slog.String("error", err.Error()))
-			c.JSON(http.StatusInternalServerError, god.H{"error": err.Error(), "message": "internal server error"})
-			return
-		}
+		h.handleError(c, err)
 	}
 
 	h.log.Debug("Successfully added new inventory item:", slog.Any("item", item))
@@ -63,38 +54,22 @@ func (h *inventoryHandler) AddInventoryItem(c *god.Context) {
 // GetInventoryItems handles the HTTP request to retrieve inventory items.
 // It calls the service layer to get the list of inventory items, handles errors, and returns the data in the response.
 func (h *inventoryHandler) GetInventoryItems(c *god.Context) {
-	defer c.Request.Body.Close()
-	items, err := h.InventoryService.RetrieveInventoryItems()
+	items, err := h.inventoryService.RetrieveInventoryItems()
 	if err != nil {
-		switch err {
-		default:
-			h.log.Error("Error of GetInventoryItems", slog.String("error", err.Error()))
-			c.JSON(http.StatusInternalServerError, god.H{"error": err.Error(), "message": "internal server error"})
-			return
-		}
+		h.handleError(c, err)
 	}
+
 	h.log.Debug("Retrieved inventory items")
 	c.JSON(http.StatusOK, god.H{"items": items})
 }
 
 // GetInventoryItem handles the HTTP request to retrieve a specific inventory item by its ID.
 func (h *inventoryHandler) GetInventoryItem(c *god.Context) {
-	defer c.Request.Body.Close()
 	itemId := c.Request.PathValue("id")
 
-	data, err := h.InventoryService.RetrieveInventoryItem(itemId)
+	data, err := h.inventoryService.RetrieveInventoryItem(itemId)
 	if err != nil {
-		switch err.Error() {
-		case "identificator is not valid":
-			c.JSON(http.StatusBadRequest, god.H{"error": err.Error(), "message": "invalid request"})
-		case "item not found":
-			c.JSON(http.StatusNotFound, god.H{"error": err.Error(), "message": fmt.Sprintf("item with id '%s' not found", itemId)})
-		default:
-			h.log.Error("Error of GetInventoryItem", slog.String("error", err.Error()))
-			c.JSON(http.StatusInternalServerError, god.H{"error": err.Error(), "message": "internal server error"})
-			return
-		}
-		return
+		h.handleError(c, err)
 	}
 
 	h.log.Debug("Retrieved inventory item with ID:", slog.String("itemId", itemId))
@@ -103,7 +78,6 @@ func (h *inventoryHandler) GetInventoryItem(c *god.Context) {
 
 // UpdateInventoryItem handles the HTTP request to update an existing inventory item by its ID.
 func (h *inventoryHandler) UpdateInventoryItem(c *god.Context) {
-	defer c.Request.Body.Close()
 	itemId := c.Request.PathValue("id")
 
 	var item models.InventoryItem
@@ -113,27 +87,9 @@ func (h *inventoryHandler) UpdateInventoryItem(c *god.Context) {
 		return
 	}
 
-	err = h.InventoryService.UpdateInventoryItem(itemId, item)
+	err = h.inventoryService.UpdateInventoryItem(itemId, item)
 	if err != nil {
-		if err.Error() == "identificator is not valid" {
-			c.JSON(http.StatusBadRequest, god.H{"error": err.Error(), "message": "invalid request body"})
-		}
-		switch err {
-		case service.ErrNoItem:
-			c.JSON(http.StatusNotFound, god.H{"error": err.Error(), "message": fmt.Sprintf("item with id '%s' not found", itemId)})
-			return
-		case service.ErrNotUniqueID,
-			service.ErrNotValidIngredientID,
-			service.ErrNotValidIngredientName,
-			service.ErrNotValidQuantity,
-			service.ErrNotValidUnit:
-			c.JSON(http.StatusBadRequest, god.H{"error": err.Error(), "message": "invalid request body"})
-			return
-		default:
-			h.log.Error("Error of UpdateInventoryItem", slog.String("error", err.Error()))
-			c.JSON(http.StatusInternalServerError, god.H{"error": err.Error(), "message": "internal server error"})
-			return
-		}
+		h.handleError(c, err)
 	}
 
 	h.log.Debug("Successfully updated an inventory item with ID:", slog.String("itemId", itemId))
@@ -141,25 +97,23 @@ func (h *inventoryHandler) UpdateInventoryItem(c *god.Context) {
 }
 
 func (h *inventoryHandler) DeleteInventoryItem(c *god.Context) {
-	defer c.Request.Body.Close()
 	itemId := c.Request.PathValue("id")
 
-	err := h.InventoryService.DeleteInventoryItem(itemId)
+	err := h.inventoryService.DeleteInventoryItem(itemId)
 	if err != nil {
-		if err.Error() == "identificator is not valid" {
-			c.JSON(http.StatusBadRequest, god.H{"error": err.Error(), "message": "invalid request body"})
-		}
-		switch err {
-		case service.ErrNoItem:
-			c.JSON(http.StatusNotFound, god.H{"error": err.Error(), "message": fmt.Sprintf("item with id '%s' not found", itemId)})
-			return
-		default:
-			h.log.Error("Error of UpdateInventoryItem", slog.String("error", err.Error()))
-			c.JSON(http.StatusInternalServerError, god.H{"error": err.Error(), "message": "internal server error"})
-			return
-		}
+		h.handleError(c, err)
 	}
 
 	h.log.Debug("Successfully deleted an inventory item with ID:", slog.String("itemId", itemId))
 	c.Status(http.StatusNoContent)
+}
+
+func (h *inventoryHandler) handleError(c *god.Context, err error) {
+	var serviceErr *service.ServiceError
+	if errors.As(err, &serviceErr) {
+		c.JSON(serviceErr.Code, serviceErr.Hash())
+	} else {
+		h.log.Error("Error of InventoryHandler", slog.String("error", err.Error()))
+		c.JSON(http.StatusInternalServerError, god.H{"error": err.Error(), "message": "internal server error"})
+	}
 }
